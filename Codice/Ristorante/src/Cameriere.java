10bc05f08cea483f0e4cfa7ee61c3cbc3bb0491b
figliolino <vincenzo.figliolino002@studenti.uniparthenope.it>
2023-12-05 17:38:45 +0100

    Ho modificato insernedo i thread

--- Codice/Ristorante/src/Cameriere.java
+++ Codice/Ristorante/src/Cameriere.java
@@ -2,106 +2,77 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Random;
+import java.util.concurrent.Semaphore;
 
-public class Cameriere
-{
-    public static void main(String [] args)
-    {
-        final int PORT_TO_CLIENT = 1313;        //port's number used for the connection to the client
-        final int PORT_TO_CHEF   = 1314;        //port's number used for the connection to the chef
-        final int MAX_WAITERS    = 5;           //number of restaurant's waiters
-        int availableWaiters = MAX_WAITERS,     //number of available waiters
-            availableSeats = randomNumber(),    //number of restaurant's available places
-            requiredSeats;                      //number of seats required by the customer
-        Menu menu;
+public class Cameriere extends Thread {
+    final int PORT_TO_CLIENT = 1313; // Port's number used for the connection to the client
+    final int PORT_TO_CHEF = 1314;   // Port's number used for the connection to the chef
+    final Semaphore postiSemaforo = new Semaphore(100);
 
-        try
-        {
-            try(Socket socketToChef = new Socket(InetAddress.getLocalHost(), PORT_TO_CHEF))
-            {
-                //object to read chef's information
-                ObjectInputStream readerToChef = new ObjectInputStream(socketToChef.getInputStream());
+    public void run() {
+        try (Socket socketToChef = new Socket(InetAddress.getLocalHost(), PORT_TO_CHEF)) {
+            // Object to read chef's information
+            ObjectInputStream readerToChef = new ObjectInputStream(socketToChef.getInputStream());
 
-                //waiter waits for the menu
-                menu = (Menu)readerToChef.readObject();
-                System.out.println("(Cameriere) Prendo il menù dallo chef");
+            while (true) {
+                // Waiter waits for the menu
+                Menu menu = (Menu) readerToChef.readObject();
+                System.out.println("(Cameriere" + Thread.currentThread().threadId() + ")" + "Prendo il menù dallo chef");
                 menu.showMenu();
 
-                //tries to create a socket with specified port's number to communicate with the customer
-                try(ServerSocket socketToClient = new ServerSocket(PORT_TO_CLIENT))
-                {
-                    //waiter waits for a customer
-                    System.out.println("(Cameriere) In attesa di clienti");
+                // Tries to create a socket with the specified port's number to communicate with the customer
+                try (ServerSocket socketToClient = new ServerSocket(PORT_TO_CLIENT)) {
+                    while (true) {
+                        // Waiter waits for a customer
+                        System.out.println("(Cameriere" + Thread.currentThread().threadId()+ ")" + "In attesa di clienti");
 
-                    //arrives a customer
-                    Socket acceptedClient = socketToClient.accept();
-                    System.out.println("(Cameriere) Prendo la prenotazione del cliente");
+                        // Arrives a customer
+                        Socket acceptedClient = socketToClient.accept();
+                        System.out.println("(Cameriere" + Thread.currentThread().threadId() + ")" + "Prendo la prenotazione del cliente");
 
-                    //objects for reading and writing over the socket
-                    BufferedReader readerToClient = new BufferedReader(new InputStreamReader(acceptedClient.getInputStream()));
-                    PrintWriter    writerToClient = new PrintWriter(acceptedClient.getOutputStream(), true);
+                        // Objects for reading and writing over the socket
+                        BufferedReader readerToClient = new BufferedReader(new InputStreamReader(acceptedClient.getInputStream()));
+                        PrintWriter writerToClient = new PrintWriter(acceptedClient.getOutputStream(), true);
 
-                    //gets how many seats has the customer required
-                    System.out.println("(Cameriere) Abbiamo " + availableSeats + " posti disponibili");
-                    requiredSeats = Integer.parseInt(readerToClient.readLine());
+                        // Gets how many seats the customer requires
+                        int requiredSeats = Integer.parseInt(readerToClient.readLine());
 
-                    //if there are enough available seats, the customer can enter the restaurant and take them
-                    if (requiredSeats <= availableSeats)
-                    {
-                        //there is one available waiter in less
-                        availableWaiters--;
+                        // If there are enough available seats, the customer can enter the restaurant and take them
+                        if (postiSemaforo.tryAcquire(requiredSeats)) {
+                            // There is one available waiter in less
+                            System.out.println("(Cameriere" + Thread.currentThread().threadId() + ")" + "Il cliente prende posto");
+                            writerToClient.println(true);
 
-                        //client's required seats are now occupied
-                        availableSeats -= requiredSeats;
-                        System.out.println("(Cameriere) Il cliente prende posto");
-                        writerToClient.println(true);
-                        System.out.println("(Cameriere) Abbiamo " + availableSeats + " posti disponibili");
+                            System.out.println("(Cameriere" + Thread.currentThread().threadId() + ")" + "Numero dei posti aggiornati:" + postiSemaforo.availablePermits());
 
-                        //the waiter sends menu to the client
-                        System.out.println("(Cameriere) Fornisco il menù al cliente");
-                       try(ObjectOutputStream menuWriter = new ObjectOutputStream(acceptedClient.getOutputStream())) {
-                           menuWriter.writeObject(menu);
-                       }
-                       catch (IOException e)
-                       {
-                           e.printStackTrace();
-                       }
+                            // Sends menu to the client
+                            System.out.println("(Cameriere" + Thread.currentThread().threadId() + ")" + "Fornisco il menù al cliente");
+                            try (ObjectOutputStream menuWriter = new ObjectOutputStream(acceptedClient.getOutputStream())) {
+                                menuWriter.writeObject(menu);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
 
-                        //the client exits the restaurant and its seats are now free
-                        acceptedClient.close();
-                        availableWaiters += requiredSeats;
+                            // The client exits the restaurant and its seats are now free
+                            postiSemaforo.release(requiredSeats);
+                        } else {
+                            System.out.println("(Cameriere) Non ci sono abbastanza posti");
+                            writerToClient.println(false);
+                        }
                     }
-                    else
-                    {
-                        System.out.println("(Cameriere) Non ci sono abbastanza posti");
-                        writerToClient.println(false);
-                        acceptedClient.close();
-                    }
-                }
-                catch (IOException exc)
-                {
+                } catch (IOException exc) {
                     System.out.println("(Server) Errore creazione socket o impossibile connettersi al cliente");
                     exc.printStackTrace();
                 }
             }
-            catch (IOException exc)
-            {
-                System.out.println("(Server) Errore creazione socket o impossibile connettersi allo chef");
-                exc.printStackTrace();
-            }
-        }
-        catch (Exception exc)
-        {
-            System.out.println("(Server) Errore interno");
+        } catch (IOException | ClassNotFoundException exc) {
+            System.out.println("(Server) Errore durante la comunicazione con lo chef");
             exc.printStackTrace();
         }
     }
 
-    //generates a random number that indicates how many seats are required by the customer
-    public static int randomNumber()
-    {
-        Random random = new Random();
-        return random.nextInt(100) + 1;
+    public static void main(String[] args) {
+        Cameriere cameriere1 = new Cameriere();
+        cameriere1.start();
     }
 }
--- Codice/Ristorante/src/Cliente.java
+++ Codice/Ristorante/src/Cliente.java
@@ -2,62 +2,64 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Random;
+import java.util.concurrent.Semaphore;
 
-public class Cliente
+public class Cliente extends Thread
 {
-    public static void main(String [] args)
+    private static final Object lock = new Object();
+    final int PORT         = 1313;                  //port's number used for the connection to the waiter
+    public void run()
     {
-        final int PORT         = 1313;               //port's number used for the connection to the waiter
         int     requiredSeats  = randomNumber();     //number of required seats
         boolean answer;                              //true if there are available seats and false otherwise
         Menu menu;
+        final Semaphore postiSemaforo = new Semaphore(100);
 
-        try
-        {
-            System.out.println("(Cliente) Attendo la disponibilità di posti");
+        try {
 
-            //tries to create a socket with specified server's address and port's number to communicate with the waiter
-            try (Socket socket = new Socket(InetAddress.getLocalHost(), PORT))
-            {
-                //objects for reading and writing over the socket
-                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+                System.out.println("(Cliente" + Thread.currentThread().getId() + ")" + "Attendo la disponibilità di posti");
 
-                //says how many seats he needs
-                System.out.println("(Cliente) Mi servono " + requiredSeats + " posti");
-                writer.println(requiredSeats);
+                //tries to create a socket with specified server's address and port's number to communicate with the waiter
+                try (Socket socket = new Socket(InetAddress.getLocalHost(), PORT)) {
+                    //objects for reading and writing over the socket
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
 
-                //gets waiter response
-                answer = Boolean.parseBoolean(reader.readLine());
+                    //Gestisco la richiesta dei posti dei vari clienti
+                    synchronized (lock) {
 
-                //if there are available seats, the customer takes them
-                if (answer)
-                {
-                    System.out.println("(Cliente) Prendo posto e richiedo il menù");
+                        postiSemaforo.acquire(requiredSeats);
+                        //says how many seats he needs
+                        System.out.println("(Cliente" + Thread.currentThread().threadId() + ")" + " Mi servono " + requiredSeats + " posti");
+                        writer.println(requiredSeats);
 
-                    try(ObjectInputStream readerObject = new ObjectInputStream(socket.getInputStream())){
-                        menu = (Menu) readerObject.readObject();
-                        System.out.println("(Cliente) Il menù è " );
-                        menu.showMenu();
+                        //gets waiter response
+                        answer = Boolean.parseBoolean(reader.readLine());
+
+                        //if there are available seats, the customer takes them
+                        if (answer) {
+                            System.out.println("(Cliente" + Thread.currentThread().threadId() + ")" + "Prendo posto e richiedo il menù");
+
+                            try (ObjectInputStream readerObject = new ObjectInputStream(socket.getInputStream())) {
+                                menu = (Menu) readerObject.readObject();
+                                System.out.println("(Cliente" + Thread.currentThread().threadId() + ")" + "Il menù è ");
+                                menu.showMenu();
+                            } catch (IOException | ClassNotFoundException e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            System.out.println("(Cliente" + Thread.currentThread().threadId() + ")" + "Me ne vado!");
+                        }
+
+                        postiSemaforo.release(requiredSeats);
                     }
-                    catch(IOException | ClassNotFoundException e)
-                    {
-                        e.printStackTrace();
-                    }
+                } catch (IOException exc) {
+                    System.out.println("(Cliente" + Thread.currentThread().threadId() + ")" + "Errore creazione socket o impossibile connettersi al server");
                 }
-                else
-                {
-                    System.out.println("(Cliente) Me ne vado!");
-                }
-            }
-            catch (IOException exc)
-            {
-                System.out.println("(Client) Errore creazione socket o impossibile connettersi al server");
-            }
         }
         catch (Exception exc)
         {
-            System.out.println("(Client) Errore interno");
+            System.out.println("(Cliente" + Thread.currentThread().threadId() + ")" + "Errore interno");
         }
     }
 
@@ -66,5 +68,13 @@ public class Cliente
     {
         Random random = new Random();
         return random.nextInt(10) + 1;
+    }
+
+    public static void main(String[] args)
+    {
+        Cliente cliente1 = new Cliente();
+        Cliente cliente2 = new Cliente();
+        cliente1.start();
+        cliente2.start();
     }
 }
